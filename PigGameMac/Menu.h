fee7254c7d914084b0778a08703bef6bdebe809f
ezequielh97 <36419172+ezequielh97@users.noreply.github.com>
2018-05-18 16:57:44 -0700

    Update Menu.h

--- PigGameMac/Menu.h
+++ PigGameMac/Menu.h
@@ -6,15 +6,16 @@ using namespace std;
 class Menu
 {
 private:
-    int gameMode; // Single player or Multiplayer
-    
+	int gameMode; // Single player or Multiplayer
+
 public:
-    
-    Menu(); // Default Constructor
-    
-    void setGameMode(int);
-    int getGameMode();
-    void display(); // Displays the menu
+
+	Menu(); // Default Constructor
+
+	void setGameMode(int);
+	int getGameMode();
+	void display(); // Displays the menu
+	int inputValidation(int);
 };
 
 
@@ -24,26 +25,43 @@ public:
 
 Menu::Menu()
 {
-    int choice;
-    display(); // Displays the menu to the user
+	int choice;
+	display(); // Displays the menu to the user
 
-    cin >> choice; // User inputs their choice here
-    gameMode = choice;
-    
-    
-    // *VALIDATE USER INPUT FOR CHOICE, USE EXCEPTION*
+	cin >> choice; // User inputs their choice here
+
+	
+	gameMode = choice;
+
+
+	// *VALIDATE USER INPUT FOR CHOICE, USE EXCEPTION*
 }
 
 void Menu::display()
 {
-    cout << "MENU:" << endl;
-    cout << "[1] - Single Player Mode" << endl;
-    cout << "[2] - Multiplayer Mode" << endl;
-    cout << "Enter Choice: ";
+	cout << "MENU:" << endl;
+	cout << "[1] - Single Player Mode" << endl;
+	cout << "[2] - Multiplayer Mode" << endl;
+	cout << "Enter Choice: ";
 }
 
 void Menu::setGameMode(int mode)
-{ gameMode = mode; }
+{
+	gameMode = mode;
+}
 
 int Menu::getGameMode()
-{ return gameMode; }
+{
+	return gameMode;
+}
+int Menu::inputValidation(int input)
+{
+	while (input < 1 || input > 2 || !cin)
+	{
+		cout << "Invalid input. Try again " << endl;
+		cin >> input;
+		cin.clear(); // reset failbit
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+	}
+	return input;
+}
