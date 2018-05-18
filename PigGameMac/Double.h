94d7a426c5480f92331d011e06b601359afd052b
ezequielh97 <36419172+ezequielh97@users.noreply.github.com>
2018-05-18 16:54:05 -0700

    Update Double.h

--- PigGameMac/Double.h
+++ PigGameMac/Double.h
@@ -1,4 +1,4 @@
-#pragma once
+##pragma once
 #include "Core.h"
 using namespace std;
 
@@ -6,20 +6,21 @@ using namespace std;
 class Double : public Core
 {
 private:
-    int roundPoints;
-    int numPlayers;
-    Core* player;
-    
+	int roundPoints;
+	int numPlayers;
+	Core* player;
+
 public:
-    Double(); // Default Constructor
-    Double(int); // Constructor
-    ~Double(); // Destructor
-    
-    void game(); // The multiplayer game happens here
-    void displayRules(); // Displays the rules to the users
-    
-    void setRoundPoints(int, int); // Sets the points for that round
-    void setPlayers(); // Gets the number of players playing
+	Double(); // Default Constructor
+	Double(int); // Constructor
+	~Double(); // Destructor
+
+	void game(); // The multiplayer game happens here
+	void displayRules(); // Displays the rules to the users
+
+	void setRoundPoints(int, int); // Sets the points for that round
+	void setPlayers(); // Gets the number of players playing
+	
 };
 
 
@@ -28,150 +29,153 @@ public:
 //******************************************************************************
 Double::Double()
 {
-    numDie = 0;
-    totalPoints = 0;
-    highScore = 0;
-    numPlayers = 2;
-    tries = 1;
-    max = 100;
-    rollOne = false;
-    turn = true;
-    
-    die = new Die[numDie];
-    snakeEyes = new bool[numDie];
-    player = new Core[numPlayers];
-    
-    for(int i = 0; i < numDie; i++)
-    {
-        snakeEyes[i] = false;
-    }
-    for(int j = 0; j < numPlayers; j++)
-    {
-        player[j] = (Core (numDie, true));
-    }
-    
+	numDie = 0;
+	totalPoints = 0;
+	highScore = 0;
+	numPlayers = 2;
+	tries = 1;
+	max = 100;
+	rollOne = false;
+	turn = true;
+
+	die = new Die[numDie];
+	snakeEyes = new bool[numDie];
+	player = new Core[numPlayers];
+
+	for (int i = 0; i < numDie; i++)
+	{
+		snakeEyes[i] = false;
+	}
+	for (int j = 0; j < numPlayers; j++)
+	{
+		player[j] = (Core(numDie, true));
+	}
+
 }
 
 Double::Double(int num)
 {
-    max = 100;
-    numDie = num;
-    setPlayers();
-    totalPoints = 0;
-    turn = true;
-    rollOne = false;
-    
-    die = new Die[numDie];
-    snakeEyes = new bool[numDie];
-    player = new Core[numPlayers];
-    
-    for(int i = 0; i < numDie; i++)
-    {
-        snakeEyes[i] = false;
-    }
-    for(int j = 0; j < numPlayers; j++)
-    {
-        player[j] = (Core(numDie, true));
-    }
-    
-    setMax();
-    displayRules();
-    game();
+	max = 100;
+	numDie = num;
+	setPlayers();
+	totalPoints = 0;
+	turn = true;
+	rollOne = false;
+
+	die = new Die[numDie];
+	snakeEyes = new bool[numDie];
+	player = new Core[numPlayers];
+
+	for (int i = 0; i < numDie; i++)
+	{
+		snakeEyes[i] = false;
+	}
+	for (int j = 0; j < numPlayers; j++)
+	{
+		player[j] = (Core(numDie, true));
+	}
+
+	setMax();
+	displayRules();
+	game();
 }
 
 Double::~Double()
 {
-    delete [] die;
-    delete [] snakeEyes;
-    
-    die = NULL;
-    snakeEyes = NULL;
+	delete[] die;
+	delete[] snakeEyes;
+
+	die = NULL;
+	snakeEyes = NULL;
 }
 
 void Double::setPlayers()
 {
-    int choice;
-    cout << "You are set on 2 player mode. Press 1 to continue or 2 to add players: ";
-    cin >> choice;
-    
-    if(choice == 1)
-        numPlayers = 2;
-    else if(choice == 2)
-    {
-        cout << "Enter the number of players: ";
-        cin >> numPlayers;
-    }
+	int choice;
+	cout << "You are set on 2 player mode. Press 1 to continue or 2 to add players: ";
+	cin >> choice;
+	choice = inputValidation(choice);
+
+	if (choice == 1)
+		numPlayers = 2;
+	else if (choice == 2)
+	{
+		cout << "Enter the number of players: ";
+		cin >> numPlayers;
+		numPlayers = bigInputValidation(numPlayers);
+	}
 }
 
 void Double::displayRules()
 {
-    cout << endl << endl;
-    
-    char anyKey;
-    
-    cout << "MULTIPLAYER RULES:" << endl;
-    cout << "During a player's turn, the player will roll their dice." << endl;
-    cout << "The numbers the dice land on will be added to the player's total." << endl;
-    if(numDie == 1)
-        cout << "If the die lands on 1, the player will lose the points accumulated for that round and lose their turn." << endl;
-    else
-    {
-        cout << "If one of the die lands on 1, the player will lose their points for that round and lose their turn." << endl;
-        cout << "If all of the dice land on 1, the player will lose ALL of their points." << endl;
-        cout << "The first person to make it to " << max << " wins." << endl;
-    }
-    
-    cout << "Press any key, then enter, to continue... ";
-    cin >> anyKey;
-    cout << endl << endl;
+	cout << endl << endl;
+
+	char anyKey;
+
+	cout << "MULTIPLAYER RULES:" << endl;
+	cout << "During a player's turn, the player will roll their dice." << endl;
+	cout << "The numbers the dice land on will be added to the player's total." << endl;
+	if (numDie == 1)
+		cout << "If the die lands on 1, the player will lose the points accumulated for that round and lose their turn." << endl;
+	else
+	{
+		cout << "If one of the die lands on 1, the player will lose their points for that round and lose their turn." << endl;
+		cout << "If all of the dice land on 1, the player will lose ALL of their points." << endl;
+		cout << "The first person to make it to " << max << " wins." << endl;
+	}
+
+	cout << "Press any key, then enter, to continue... ";
+	cin >> anyKey;
+	cout << endl << endl;
 }
 
 void Double::game()
 {
-    int count; // To keep a count of total points for subtraction later to get roundPoints
-    for(int i = 0; i < numPlayers; i++) // For loop for each player's turn
-    {
-        count = player[i].getTotalPoints();
-        turn = true;
-        cout << "PLAYER " << i + 1 << endl;
-        do
-        {
-            
-            player[i].Turn();
-            setRoundPoints(i, count);
-            
-            if(player[i].getRollOne() == true && (player[i].SnakeEyes() == false || player[i].getNumDie() == 1))
-            {
-                cout << "One of your die landed on 1, you lose your turn!" << endl;
-                cout << "You lost your " << roundPoints << " points. ";
-                player[i].setTotalPoints(count);
-                cout << "Your total is currently: " << player[i].getTotalPoints() << endl;
-                player[i].setTurn(false);
-            }
-            else if(player[i].getRollOne() == true && player[i].SnakeEyes() == true && player[i].getNumDie() != 1)
-            {
-                cout << "All of your dice landed on 1! You lose all of  your points." << endl;
-                player[i].setTotalPoints(0);
-                player[i].setTurn(false);
-            }
-            else if(player[i].getRollOne() == false && player[i].SnakeEyes() == false)
-            {
-                cout << "CURRENT POINTS: " << roundPoints << endl;
-                cout << "TOTAL POINTS: " << player[i].getTotalPoints() << endl;
-                player[i].rollAgain();
-            }
-            cout << endl;
-        }while(player[i].getTurn() == true); // End of do-while loop
-        count = NULL;
-        roundPoints = NULL;
-        
-        
-        if(i == (numPlayers - 1)) // If it gets to the last player, i becomes -1 so that it become 0 when the loop iterates
-            i = -1;
-    } // End of the for loop
+	int count; // To keep a count of total points for subtraction later to get roundPoints
+	for (int i = 0; i < numPlayers; i++) // For loop for each player's turn
+	{
+		count = player[i].getTotalPoints();
+		turn = true;
+		cout << "PLAYER " << i + 1 << endl;
+		do
+		{
+
+			player[i].Turn();
+			setRoundPoints(i, count);
+
+			if (player[i].getRollOne() == true && (player[i].SnakeEyes() == false || player[i].getNumDie() == 1))
+			{
+				cout << "One of your die landed on 1, you lose your turn!" << endl;
+				cout << "You lost your " << roundPoints << " points. ";
+				player[i].setTotalPoints(count);
+				cout << "Your total is currently: " << player[i].getTotalPoints() << endl;
+				player[i].setTurn(false);
+			}
+			else if (player[i].getRollOne() == true && player[i].SnakeEyes() == true && player[i].getNumDie() != 1)
+			{
+				cout << "All of your dice landed on 1! You lose all of  your points." << endl;
+				player[i].setTotalPoints(0);
+				player[i].setTurn(false);
+			}
+			else if (player[i].getRollOne() == false && player[i].SnakeEyes() == false)
+			{
+				cout << "CURRENT POINTS: " << roundPoints << endl;
+				cout << "TOTAL POINTS: " << player[i].getTotalPoints() << endl;
+				player[i].rollAgain();
+			}
+			cout << endl;
+		} while (player[i].getTurn() == true); // End of do-while loop
+		count = NULL;
+		roundPoints = NULL;
+
+
+		if (i == (numPlayers - 1)) // If it gets to the last player, i becomes -1 so that it become 0 when the loop iterates
+			i = -1;
+	} // End of the for loop
 }
 
 void Double::setRoundPoints(int j, int count)
 {
-    roundPoints = player[j].getTotalPoints() - count;
+	roundPoints = player[j].getTotalPoints() - count;
 }
+
